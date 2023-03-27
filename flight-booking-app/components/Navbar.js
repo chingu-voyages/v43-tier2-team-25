4a8651471b00f97a07782f6f19418c1f3cb7724d
Dytoma <dytomaomar@gmail.com>
2023-03-27 20:38:59 +0000

    change the navbar

--- flight-booking-app/components/Navbar.js
+++ flight-booking-app/components/Navbar.js
@@ -2,113 +2,114 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/Assets/Images/shared/logo.svg'
-import closeIcon from '../public/Assets/Images/shared/icon-x.svg'
-import hamburger from '../public/Assets/Images/shared/Hamburger_icon.svg.png'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr'
 
 
 const Navbar = () => {
 
-  const [ navbar, setNavbar ] = useState(false)
+    const [navbar, setNavbar] = useState(false)
 
-  return (
-    <div>
+    return (
+        <div>
 
-      <nav className='w-full fixed top-0 left-0 right-0 z-10'>
-        
-
-        <div className='justify-around px-4 mx-auto md:flex md:items-center md:justify-between md:lg:max-w-7xl md:px-8 '> 
-          
-          <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-
-            {/* Logo */}
-            <Link href='/'>
-
-              <Image  src={logo} alt='Flighty Logo' width={100} height={100} />
-
-            </Link>
+            <nav className='w-full lg:pt-16 md:pt-10 pt-7'>
 
 
-            {/* Hamburger Button For Mobile */}
-            <div className=''>
-      
-              <div className='md:hidden'>
+                <div className='justify-around px-4 md:flex md:items-center md:justify-between md:mx-[13.25rem] md:px-8 '>
 
-              
+                    <div className='flex items-center justify-between py-3 md:py-5 md:block'>
 
-                <button className='p-2 text text-gray-700 rounded-md outline-none focus:border-gray-400' onClick={
-                  () => setNavbar(!navbar)
-                }>
+                        {/* Logo */}
+                        <Link href='/'>
 
-                  {
-                    navbar ? (
-                      <Image src={closeIcon} width={30} height={30} alt='closeNavbar' />
-                    ) : (
-                      <Image src={hamburger} width={30} height={30} alt='hamburger' />
-                    )
-                  }
+                            <Image src={logo} alt='Flighty Logo' className='lg:h-11 md:h-8 h-6 z-20' />
 
-                </button>
-
-              </div>
-
-            </div>
-
-          </div>
-
-          <div className={`flex-1 justify-self-center items-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-          navbar ? 'p-12 md:p-0 block' : 'hidden'
-          }`}>
-
-          <ul className='h-screen md:h-auto items-center justify-center md:flex md:items-center'>
-
-            <div className='md:flex md:ml-auto'>
+                        </Link>
 
 
+                        {/* Hamburger Button For Mobile */}
+                        <div className='md:hidden'>
 
-            <li className='pb-6 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0 md:mt-6'> 
 
-            <Link href='/' onClick={() => setNavbar(!navbar)}> Home</Link> 
-            
-            </li>
+                            <button className='p-2 text-2xl text-grayText rounded-md outline-none focus:border-gray-400' onClick={
+                                () => setNavbar(true)
+                            }>
 
-            <li className='pb-6 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0 md:mt-6'> 
+                                <GiHamburgerMenu alt='hamburger' />
 
-            <Link href='/explore' onClick={() => setNavbar(!navbar)}> Explore</Link> 
-            
-            </li>
+                            </button>
 
-            <li className='pb-6 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0 md:mt-6'> 
+                        </div>
 
-            <Link href='/services' onClick={() => setNavbar(!navbar)}> Services</Link> 
-            
-            </li>
+                    </div>
 
-            </div>
-            
-            <div className='text-center md:text-left md:flex md:items-center md:w-6/12'>
+                    <div className={`fixed md:static top-0 bottom-0 left-0 right-0 bg-black/50 lg:bg-transparent lg:backdrop-blur-none backdrop-blur-xl z-10  items-center pb-3 md:block ${navbar ? 'p-12 md:p-0 block' : 'hidden'
+                        }`}>
 
-              <button className='bg-primaryBlue px-5 py-2 mt-10 rounded-lg text-white md:ml-auto md:my-auto md:mt-6'> 
-                          
-                <Link href='#'>Sign Up</Link>
-                                      
-              </button>
+                        <ul className='h-full md:h-auto items-center justify-center md:flex bg-white md:bg-transparent'>
 
-            </div>
-          
 
-          </ul>
+                            <li className='md:hidden flex justify-end'>
+                                <button className='p-2 text-2xl text-grayText rounded-md outline-none focus:border-gray-400' onClick={
+                                    () => setNavbar(false)
+                                }>
 
-          
-          
-          
+                                    <GrClose alt='hamburger' />
+
+                                </button>
+
+                            </li>
+
+                            <li className='pb-6 md:pb-0 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+
+                                <Link href='/' onClick={() => setNavbar(!navbar)}> Home</Link>
+
+                            </li>
+
+                            <li className='pb-6 md:pb-0 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+
+                                <Link href='/explore' onClick={() => setNavbar(!navbar)}> Explore</Link>
+
+                            </li>
+
+                            <li className='pb-6 md:pb-0 text-xl text-grayText py-2 md:px-6 text-center border-b-2 md:border-b-0'>
+
+                                <Link href='/services' onClick={() => setNavbar(!navbar)}> Services</Link>
+
+                            </li>
+                            <li className=' md:hidden grid items-center'>
+
+                                <button className='bg-primaryBlue px-5 py-2 mt-10 rounded-lg text-lightColor md:mt-0'>
+
+                                    <Link href='#'>Sign Up</Link>
+
+                                </button>
+
+                            </li>
+
+
+                        </ul>
+
+                    </div>
+
+
+                    <div className='md:block hidden'>
+
+                        <button className='bg-primaryBlue px-5 py-2 mt-10 rounded-lg text-lightColor md:mt-0'>
+
+                            <Link href='#'>Sign Up</Link>
+
+                        </button>
+
+                    </div>
+
+                </div>
+
+
+            </nav>
         </div>
-
-        </div>
-       
-
-      </nav>
-    </div>
-  )
+    )
 
 }
 
--- flight-booking-app/pages/index.js
+++ flight-booking-app/pages/index.js
@@ -1,16 +1,35 @@
-import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-
 
 
 export default function Home() {
   return (
     <>
       <div className={styles.main}>
-        
+        <h1 className='text-2xl text-blue-500'>The app is up and running</h1>
+        <h2 className='text-xl text-blue-900'>Let's Go🚀🚀</h2>
       </div>
     </>
   )
 }
+
+db.products.insertOne(
+  {
+    _id: 2,
+    "name": "Pencil",
+    "price": 0.80,
+    "reviews": [
+      {
+        "authorName": "Patrick",
+        "rating": 5,
+        "review": "This is one of my favorite pencil!"
+      },
+      {
+        "authorName": "Omer",
+        "rating": 5,
+        "review": "Awesome product!"
+      }
+    ]
+  }
+)
