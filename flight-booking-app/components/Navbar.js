d9dd89a0b1b6a2628735b5ce768a43cb5d6f4ac0
Dytoma <dytomaomar@gmail.com>
2023-04-08 13:11:58 +0000

    Implement the code for the explore page

--- flight-booking-app/components/Navbar.js
+++ flight-booking-app/components/Navbar.js
@@ -1,21 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import Link from 'next/link'
 import Image from 'next/image'
+
 import logo from '../public/Assets/Images/shared/logo.svg'
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { GrClose } from 'react-icons/gr'
+import { GrClose } from 'react-icons/gr';
+import { BsCart3 } from 'react-icons/bs';
 
 import styles from '../styles/Navbar.module.css';
 import { motion } from 'framer-motion';
 
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { AppContext } from '@/pages/_app';
+import Saved from './Saved'
+
 const Navbar = () => {
+    const provider = new GoogleAuthProvider();
+    const router = useRouter();
 
     const [navbar, setNavbar] = useState(false);
-    const [linkActive, setLinkActive] = useState("home");
+    const [logoutBtn, setLogoutBtn] = useState(false);
+    const [user, setUser] = useState(null)
+    const { setLoggedIn, loggedIn, setShowItemsSaved, showItemsSaved, count, setCount, hotelsAdded, setHotelsAdded, flightsAdded, setFlightsAdded } = useContext(AppContext);
+
+    const auth = getAuth();
+
+    const authStateChangeHandler = (authState) => {
+        if (!authState) {
+            setLoggedIn(false)
+        } else {
+            setLoggedIn(true);
+        }
+    }
+
+    //Checking if the user has signed up
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(authStateChangeHandler)
+
+        return () => {
+            unsubscribe()
+        }
+
+    }, [])
+
+    const handleLogin = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // The signed-in user info.
+                setLoggedIn(true);
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+                // ...
+            }).catch((error) => {
+                console.log(error);
+                // ...
+            });
+    }
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                setLoggedIn(false)
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
 
     return (
         <div>
-
+            {showItemsSaved && <Saved />}
             <nav className='w-full lg:pt-16 md:pt-10 pt-7'>
 
 
@@ -68,39 +124,61 @@ const Navbar = () => {
 
                             </li>
 
-                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${linkActive === "home" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
+                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${router.pathname === "/" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
 
                                 <Link href='/' onClick={() => {
                                     setNavbar(!navbar);
-                                    setLinkActive("home")
                                 }}> Home</Link>
 
                             </li>
 
-                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${linkActive === "explore" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
+                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${router.pathname === "/explore" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
 
                                 <Link href='/explore' onClick={() => {
                                     setNavbar(!navbar);
-                                    setLinkActive("explore")
                                 }}> Explore</Link>
 
                             </li>
 
-                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${linkActive === "services" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
+                            <li className={`pb-6 md:pb-0 text-xl text-grayText ${router.pathname === "/services" ? "text-primaryBlue" : ""} py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}>
 
                                 <Link href='/services' onClick={() => {
                                     setNavbar(!navbar);
-                                    setLinkActive("services")
                                 }}> Services</Link>
 
                             </li>
+
+                            {loggedIn &&
+                                <li className={`pb-6 md:pb-0 text-xl text-grayText lg:hidden py-2 md:py-0 text-center border-b-2 md:border-b-0 ${styles.navLink}`}
+                                    onClick={() => {
+                                        handleLogout();
+                                        setNavbar(!navbar);
+                                    }}
+                                >
+                                    Sign out
+                                </li>
+                            }
+
                             <li className=' md:hidden grid items-center'>
 
-                                <button className='bg-primaryBlue px-5 py-2 mt-10 hover:opacity-75 rounded-lg text-lightColor md:mt-0'>
-
-                                    <Link href='#'>Sign Up</Link>
-
-                                </button>
+                                {loggedIn ? (
+                                    <div className='flex gap-8 items-center md:justify-start justify-center pt-4 md:pt-0'>
+                                        <div className='relative'>
+                                            <BsCart3 title='Saved' className='text-3xl text-grayText' onClick={() => {
+                                                setShowItemsSaved(true);
+                                                setNavbar(false);
+                                            }} />
+                                            {count && <p className={`rounded-full text-xs absolute -top-[6px] -right-[6px] px-[6px] py-[1px] bg-redLight text-white font-semibold ${styles.notifCart}`}>{count}</p>}
+                                        </div>
+                                        <div className='rounded-full border-2 border-grayText'>
+                                            <img src={user?.photoURL} alt={user?.displayName} width={36} height={36} className='w-9 h-9 rounded-full' />
+                                        </div>
+                                    </div>
+                                ) : <button className='bg-primaryBlue px-5 py-2 mt-10 hover:opacity-75 rounded-lg text-lightColor md:mt-0'
+                                    onClick={handleLogin}
+                                >
+                                    Sign Up
+                                </button>}
 
                             </li>
 
@@ -112,11 +190,34 @@ const Navbar = () => {
 
                     <div className='lg:block hidden md:flex gap-6 lg:gap-0'>
 
-                        <button className={`bg-primaryBlue lg:px-5 lg:py-2 md:px-4 md:py-[6px] mt-10 rounded-lg text-lightColor md:mt-0 md:text-sm lg:text-xl font-bold ${styles.btnBlue}`}>
-
-                            <Link href='#'>Sign Up</Link>
-
-                        </button>
+                        {loggedIn ? (
+                            <div className='flex gap-8 items-center'>
+                                <div className='relative'>
+                                    <BsCart3 title='Saved' className='text-3xl text-grayText' onClick={() => {
+                                        setShowItemsSaved(true);
+                                        setNavbar(false);
+                                    }} />
+                                    {count && <p className={`rounded-full text-xs absolute -top-[6px] -right-[6px] px-[6px] py-[1px] bg-redLight text-white font-semibold ${styles.notifCart}`}>{count}</p>}
+                                </div>
+                                <div className='rounded-full border-2 border-grayText' onClick={() => { setLogoutBtn(!logoutBtn) }}>
+                                    <img src={user?.photoURL} alt={user?.displayName} width={36} height={36} className='w-9 h-9 rounded-full' />
+                                </div>
+                                {logoutBtn &&
+                                    <motion.button className='bg-white px-8 py-4 absolute top-[8.375rem] right-[13.25rem] hover:opacity-75 rounded-lg text-grayishDark md:mt-0 hidden lg:block'
+                                        onClick={handleLogout}
+                                        initial={{ opacity: 0 }}
+                                        whileInView={{ opacity: 1, scale: [0, 1] }}
+                                        transition={{ duration: 0.5, ease: 'easeInOut' }}
+                                    >
+                                        Sign Out
+                                    </motion.button>
+                                }
+                            </div>
+                        ) :
+                            <button className={`bg-primaryBlue lg:px-5 lg:py-2 md:px-4 md:py-[6px] mt-10 rounded-lg text-lightColor md:mt-0 md:text-sm lg:text-xl font-bold ${styles.btnBlue}`} onClick={handleLogin}>
+                                Sign Up
+                            </button>
+                        }
                         <div className='lg:hidden'>
 
 
